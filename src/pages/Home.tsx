e37beef5ca95d858203259c5ab13741efb9d8d5c
BELKACEM Oussama <belkacem-oussama@mbp-de-belkacem.home>
2023-07-28 20:26:55 +0200

    add element to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,22 @@
+import ConjugEasyTitle from '.././assets/images/ConjugEasy.png'
+import ConjugEasyNotes from '.././assets/images/ConjugEasy_Notes.png'
+import ConjugEasyBag from '.././assets/images/ConjugEasy_Bag.png'
+import Button from '../components/Button.tsx'
+
 export default function Home() {
-    return <div>'Home'</div>
+    const text: String[] = ['Élève', 'Professeur', 'Visiteur']
+    const buttons = text.map((label, index) => (
+        <Button key={index} size="small" color="primary">
+            {label}
+        </Button>
+    ))
+
+    return (
+        <div className="home">
+            <img src={ConjugEasyTitle} alt="ConjugEasy Title" />
+            {buttons}
+            <img src={ConjugEasyNotes} alt="ConjugEasy Notes" />
+            <img src={ConjugEasyBag} alt="ConjugEasy Bag" />
+        </div>
+    )
 }
