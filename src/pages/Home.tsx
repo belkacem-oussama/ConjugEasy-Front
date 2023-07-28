58203ba8599f00390e7a79f5e8f94024aeea1e61
BELKACEM Oussama <belkacem-oussama@mbp-de-belkacem.home>
2023-07-28 19:58:28 +0200

    clear homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,3 @@
-import Button from '../components/Button.tsx'
-
 export default function Home() {
-    return (
-        <div>
-            <Button color="primary" size="small">
-                Je passe la ceinture noire
-            </Button>
-            <Button color="primary" size="medium">
-                Je m'exerce
-            </Button>
-            <Button color="primary" size="large">
-                Je consulte mes résultats
-            </Button>
-            <Button color="primary" size="large">
-                Eleve
-            </Button>
-            <Button color="primary" size="large">
-                Professeur
-            </Button>
-            <Button color="primary" size="large">
-                Visiteur
-            </Button>
-        </div>
-    )
+    return <div>'Home'</div>
 }
