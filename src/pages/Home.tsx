d5baeabb7a11f6a410edb7d23cdd6e7602435310
BELKACEM Oussama <belkacem-oussama@mbp-de-belkacem.home>
2023-07-28 20:45:37 +0200

    responsive buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,31 @@
+import Button from '../components/Button.tsx'
+
 import ConjugEasyTitle from '.././assets/images/ConjugEasy.png'
 import ConjugEasyNotes from '.././assets/images/ConjugEasy_Notes.png'
 import ConjugEasyBag from '.././assets/images/ConjugEasy_Bag.png'
-import Button from '../components/Button.tsx'
+
+import '.././assets/styles/pages/home.scss'
 
 export default function Home() {
-    const text: String[] = ['Élève', 'Professeur', 'Visiteur']
-    const buttons = text.map((label, index) => (
+    const firstTwoButtonsText: String[] = ['Élève', 'Professeur']
+    const firstTwoButtons = firstTwoButtonsText.map((label, index) => (
         <Button key={index} size="small" color="primary">
             {label}
         </Button>
     ))
 
+    const thirdButtonText = 'Visiteur'
+    const thirdButton = (
+        <Button size="small" color="primary">
+            {thirdButtonText}
+        </Button>
+    )
+
     return (
         <div className="home">
             <img src={ConjugEasyTitle} alt="ConjugEasy Title" />
-            {buttons}
+            <div className="home-buttons-top">{firstTwoButtons}</div>
+            <div className="home-button-bottom">{thirdButton}</div>
             <img src={ConjugEasyNotes} alt="ConjugEasy Notes" />
             <img src={ConjugEasyBag} alt="ConjugEasy Bag" />
         </div>
