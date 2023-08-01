d410bd44406a24ce04c3769375ac12b5964ec602
BELKACEM Oussama <belkacem-oussama@MacBook-Pro-de-BELKACEM.local>
2023-08-01 17:09:21 +0200

    add route login

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import Home from './pages/Home.tsx'
 import Footer from './layouts/Footer.tsx'
 
 import './assets/styles/main.scss'
+import Login from './pages/Login.tsx'
 
 export default function App() {
     return (
         <React.Fragment>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
             <Footer />
         </React.Fragment>
