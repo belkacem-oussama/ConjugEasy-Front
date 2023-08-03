03974e68d4ef5f4ecde7b4e1883d2f3fcbe9a2c4
BELKACEM Oussama <belkacem-oussama@MacBook-Pro-de-BELKACEM.local>
2023-08-03 17:53:53 +0100

    add password and inputValue states

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,14 +11,20 @@ interface InputProps {
 
 export default function Input({ isPassword }: InputProps) {
     const [inputValue, setInputValue] = useState<string>('')
+    const [passwordValue, setPasswordValue] = useState<string>('')
 
     return (
         <>
             <InputGroup className="input-component">
                 <Form.Control
                     type={isPassword ? 'password' : 'text'}
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)}
+                    placeholder={isPassword ? 'Mot de passe' : 'Identifiant'}
+                    value={isPassword ? passwordValue : inputValue}
+                    onChange={(e) =>
+                        isPassword
+                            ? setPasswordValue(e.target.value)
+                            : setInputValue(e.target.value)
+                    }
                 />
             </InputGroup>
         </>
