c4fd6938aaa0ad2bf7290be0b618d962ffa60d8e
BELKACEM Oussama <belkacem-oussama@MacBook-Pro-de-BELKACEM.local>
2023-08-03 20:03:21 +0100

    add header

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,11 @@
+import ConjugEasyTop from '../assets/images/ConjugEasy_Top.png'
+
+import '../assets/styles/layouts/header.scss'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <img src={ConjugEasyTop} />
+        </div>
+    )
+}
