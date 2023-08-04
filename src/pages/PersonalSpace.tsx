327334ec0770b1fe6e2b0d53de5eaec6351dc3b3
BELKACEM Oussama <belkacem-oussama@MacBook-Pro-de-BELKACEM.local>
2023-08-04 23:04:51 +0100

    integration small screens

--- src/pages/PersonalSpace.tsx
+++ src/pages/PersonalSpace.tsx
@@ -1,3 +1,21 @@
+import Button from '../components/Button.tsx'
+import '../assets/styles/pages/personal-space.scss'
+
 export default function PersonalSpace() {
-    return <div>PersonalSpace</div>
+    const textButton = [
+        'Je passe la ceinture noire',
+        'Je consulte mes résultats',
+        "Je m'exerce",
+    ]
+
+    return (
+        <div className="personal-space-container">
+            <p>Salut Amel ! Alors, que veux-tu faire aujourd'hui ? </p>
+            <div className="personal-space-container-main">
+                <Button color="primary" text={textButton[0]} />
+                <Button color="primary" text={textButton[1]} />
+                <Button color="primary" text={textButton[2]} />
+            </div>
+        </div>
+    )
 }
