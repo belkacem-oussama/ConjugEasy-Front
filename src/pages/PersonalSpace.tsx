b3751f8c9f7de1a4e2f034e9b9be64852e009934
BELKACEM Oussama <belkacem-oussama@mbp-de-belkacem.home>
2023-09-06 21:12:06 +0200

    add logout button + display good belt

--- src/pages/PersonalSpace.tsx
+++ src/pages/PersonalSpace.tsx
@@ -1,28 +1,65 @@
-import Button from '../components/Button.tsx'
-import ConjugEasyBrownBelt from '../assets/images/belts/ConjugEasy_BrownBelt.png'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+
+import Button from '../components/Button.tsx'
+
+import ConjugEasyBlackBelt from '../assets/images/belts/ConjugEasy_BlackBelt.png'
+import ConjugEasyBrownBelt from '../assets/images/belts/ConjugEasy_BrownBelt.png'
+import ConjugEasyPurpleBelt from '../assets/images/belts/ConjugEasy_PurpleBelt.png'
+import ConjugEasyBlueBelt from '../assets/images/belts/ConjugEasy_BlueBelt.png'
+import ConjugEasyWhiteBelt from '../assets/images/belts/ConjugEasy_WhiteBelt.png'
+
 import '../assets/styles/pages/personal-space.scss'
 import '../assets/styles/images/belt.scss'
-import { useState } from 'react'
 
 export default function PersonalSpace() {
     const [passedExercices, setPassedExercices] = useState<number>(0)
     const [readyToExam, setReadyToExam] = useState<boolean>(false)
+    const [currentBelt, setCurrentBelt] = useState<string>('WhiteBelt')
 
     const textButton: string[] = [
         'Je passe la ceinture noire',
         "Je m'entraîne",
         'Je consulte mes résultats',
         "Je m'exerce",
+        'Se deconnecter',
     ]
 
-    const beltOrder: string[] = ['White', 'Blue', 'Purple', 'Brown', 'Black']
+    const beltOrder: object = {
+        1: 'White',
+        2: 'Blue',
+        3: 'Purple',
+        4: 'Brown',
+        5: 'Black',
+    }
+
+    let beltToDisplay: string = ''
+
+    switch (currentBelt) {
+        case 'WhiteBelt':
+            beltToDisplay = ConjugEasyWhiteBelt
+            break
+        case 'BlueBelt':
+            beltToDisplay = ConjugEasyBlueBelt
+            break
+        case 'PurpleBelt':
+            beltToDisplay = ConjugEasyPurpleBelt
+            break
+        case 'BrownBelt':
+            beltToDisplay = ConjugEasyBrownBelt
+            break
+        case 'BlackBelt':
+            beltToDisplay = ConjugEasyBlackBelt
+            break
+        default:
+            break
+    }
 
     return (
         <div className="personal-space-container">
             <p>Salut Amel ! Alors, que veux-tu faire aujourd'hui ? </p>
             <p>Ceinture actuelle :</p>
-            <img src={ConjugEasyBrownBelt} id="belt" />
+            <img src={beltToDisplay} id="belt" />
             <div className="personal-space-container-main">
                 <div className="personal-space-container-main-top">
                     <Link to="/start">
@@ -42,6 +79,7 @@ export default function PersonalSpace() {
                 </div>
                 <div className="personal-space-container-main-bottom">
                     <Button color="primary" text={textButton[3]} />
+                    <Button color="primary" text={textButton[4]} />
                 </div>
             </div>
         </div>
