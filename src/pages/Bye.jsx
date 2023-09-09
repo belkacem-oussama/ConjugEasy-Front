b4a9d442eda41cc1d1f14c94ed0534192a20f0a7
BELKACEM Oussama <belkacem-oussama@mbp-de-belkacem.home>
2023-09-09 06:39:18 +0200

    replace image remove bg + structure bye-bye page

--- src/pages/Bye.jsx
+++ src/pages/Bye.jsx
@@ -1,5 +1,22 @@
-import React from 'react'
+import Button from '../components/Button.jsx'
+
+import ConjugEasyBye from '../assets/images/bye/ConjugEasy_Bye.png'
+import ConjugEasyHome from '../assets/images/home/ConjugEasy_Cahier.png'
+
+import '../assets/styles/pages/bye.scss'
 
 export default function Bye() {
-    return <div>Bye</div>
+    return (
+        <div className="bye-page">
+            <div className="bye-page-container">
+                <h1>Ohh ! Il est déjà 16h30 ? Salut! </h1>
+                <img src={ConjugEasyBye} />
+                <Button
+                    color="secondary"
+                    text="Retour à l'accueil"
+                    img={ConjugEasyHome}
+                />
+            </div>
+        </div>
+    )
 }
