2261765078a2da6681732581963dd8fe015e364a
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2023-10-02 15:50:20 +0200

    fix bugs

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -94,10 +94,10 @@ export default function Square({
             {location.pathname === '/result' ? (
                 <div className="square-tablet-result">
                     <h1>Score :</h1>
-                    {goodScore !== null ? (
-                        <h2>{goodScore}/5</h2>
-                    ) : (
+                    {goodScore === null || isNaN(goodScore) ? (
                         <h2>Oups...une erreur est survenue</h2>
+                    ) : (
+                        <h2>{goodScore}/5</h2>
                     )}
                     <p>{textToDisplay}</p>
                 </div>
