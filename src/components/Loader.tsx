320f4a4e75a159e279ada533f478fd038198fae7
BELKACEM Oussama <belkacem-oussama@MacBook-Pro-de-BELKACEM.local>
2023-10-23 15:00:26 +0200

    type for Loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import Loader from 'react-loaders'
 
-export default function LoaderComponent() {
+interface LoaderComponentProps {
+    type: string
+    active: boolean
+}
+
+export default function LoaderComponent({
+    type,
+    active,
+}: LoaderComponentProps) {
     return (
         <>
             <Loader type="pacman" active />
